fdbcdc221497fdc8261eeda6d265293df85f78e9
Vladan Jovicic <vladan94.jovicic@gmail.com>
2016-03-25 18:47:11 +0100

    more user friendly

--- src/com/jovicic/vladan/Main.java
+++ src/com/jovicic/vladan/Main.java
@@ -1,42 +1,92 @@
 package com.jovicic.vladan;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Vector;
+import java.util.function.IntBinaryOperator;
 
 /**
  * Created by vlada on 2/26/2016.
  */
 public class Main {
     private static rReadibilityCalc calc;
+    private static Graph g;
+    private static String outputGraph, outputVertices;
+    private static int exact = -1;
     public static void main(String [] args)
     {
-        Graph g;/* = new Graph(12);
-        g.addEdge(0,6);
-        g.addEdge(0,7);
-        g.addEdge(1,6);
-        g.addEdge(1,8);
-        g.addEdge(2,6);
-        g.addEdge(2,7);
-        g.addEdge(2,8);
-        g.addEdge(2,9);
-        g.addEdge(3,7);
-        g.addEdge(3,9);
-        g.addEdge(3,10);
-        g.addEdge(4,8);
-        g.addEdge(4,9);
-        g.addEdge(4,11);
-        g.addEdge(5,9);
-        g.addEdge(5,10);
-        g.addEdge(5,11);*/
-        GridGraph gridg = new GridGraph(3,4);
-        g = gridg.getGridGraph();
-
-        ReadibilityCalculator rb = new ReadibilityCalculator(g,4,2);
-        if (rb.isReadibilityExactly(3, true)) {
-            g.printGraphToFile("inputGraph.txt");
-            g.printVerticesToFile("graphRandom.txt");
-        } else {
-            System.out.println("Upper bound should be greater");
+        g = choose();
+        if(g != null)
+        {
+            ReadibilityCalculator rb = new ReadibilityCalculator(g, 4, 2);
+            if(exact > 1)
+            {
+                if(rb.isReadibilityExactly(exact, true))
+                {
+                    g.printVerticesToFile(outputVertices);
+                    if(outputGraph != null && outputGraph != "")
+                    {
+                        g.printGraphToFile(outputGraph);
+                    }
+                }
+            }
+            else
+            {
+                rb.calculate();
+                g.printVerticesToFile(outputVertices);
+                if(outputGraph != null && outputGraph != "")
+                {
+                    g.printGraphToFile(outputGraph);
+                }
+            }
         }
 
+    }
+
+    public static Graph choose()
+    {
+        Graph g = null;
+        System.out.println("1. Random graph with given probability");
+        System.out.println("2. Grid graph with given size");
+        System.out.println("3. Graph defined in a file");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Type your choice: ");
+        try {
+            int c = Integer.parseInt(in.readLine());
+            if(c == 1)
+            {
+                System.out.print("Enter number of vertices n = ");
+                int n = Integer.parseInt(in.readLine());
+                System.out.print("Probability for an edge p = ");
+                int p = Integer.parseInt(in.readLine());
+                g = new RandomGraph(n,p).getNewRandomGraph();
+                //return g;
+            }
+            else if(c == 2)
+            {
+                System.out.print("Enter width:");
+                int w = Integer.parseInt(in.readLine());
+                System.out.print("Enter height:");
+                int h = Integer.parseInt(in.readLine());
+                g = new GridGraph(w,h).getGridGraph();
+                //return g;
+            }
+            else
+            {
+                System.out.println("Not available! Try again");
+                return null;
+            }
+            System.out.print("Enter name of a file to output: ");
+            outputVertices = in.readLine();
+            System.out.println("Enter name of file to output edges (leave blank if you do not want to output)");
+            outputGraph = in.readLine();
+            System.out.println("Type -1 if you want to calculate by algorithm or enter value if you want to check for exact readability");
+            exact = Integer.parseInt(in.readLine());
+        } catch (IOException e)
+        {
+            System.out.println("Wrong input!");
+        }
+        return g;
     }
 }
