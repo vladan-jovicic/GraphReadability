3a9fcf06ce248dd59ab98009e107af72f881600d
Vladan Jovicic <vladan94.jovicic@gmail.com>
2016-04-21 11:17:58 +0200

    corrected bisection

--- src/com/jovicic/vladan/ReadibilityCalculator.java
+++ src/com/jovicic/vladan/ReadibilityCalculator.java
@@ -29,15 +29,25 @@ public class ReadibilityCalculator {
     {
         rReadibilityCalc calc;
         int lo = lb, hi = ub;
-        while(lo<hi)
+        boolean found [] = new boolean[ub+1];
+        boolean calculated [] = new boolean[ub+1];
+        for(int i=0; i<ub+1; i++) {
+            found[i] = false;
+            calculated[i] = false;
+        }
+        while(lo<=hi)
         {
             int mid = (lo+hi)/2;
+            if(calculated[mid])
+                break;
+            calculated[mid] = true;
             System.out.println("Provjeravam za "+mid);
             calc = new rReadibilityCalc(g, mid);
             int result = calc.isrReadibility();
             if(result == 1)
             {
-                hi = mid;
+                found[mid] = true;
+                hi = mid-1;
             }
             else if(result == 0)
             {
@@ -49,8 +59,8 @@ public class ReadibilityCalculator {
                 lo = mid+1;
             }
         }
-        calc = new rReadibilityCalc(g, hi);
-        if (calc.isrReadibility() == 1)
+        //calc = new rReadibilityCalc(g, hi);
+        /*if (calc.isrReadibility() == 1)
         {
             //nasao sam ga
             System.out.println("Lower bound: " + hi);
@@ -61,7 +71,23 @@ public class ReadibilityCalculator {
         else
         {
             return false;
+        }*/
+        boolean isInterval = false;
+        for(int i=1; i<ub+1; i++)
+        {
+            if(found[i] == true)
+            {
+                isInterval = true;
+                System.out.println("Lower bound: " + i);
+                //g.setReadability(i);
+                //calc.setLabeling(g);
+                break;
+            }
         }
+        if(isInterval)
+            return true;
+        else
+            return false;
     }
     public boolean isReadibilityExactly(int r, boolean label)
     {
